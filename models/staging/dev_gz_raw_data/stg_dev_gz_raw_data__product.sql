5d48f5afbaf6a6f0b4a4c2625f4733f2e9617cec
duval.ekaterina@gmail.com <duval.ekaterina@gmail.com>
2025-11-06 10:52:41 +0000

    calcul purchase_cost

--- models/staging/dev_gz_raw_data/stg_dev_gz_raw_data__product.sql
+++ models/staging/dev_gz_raw_data/stg_dev_gz_raw_data__product.sql
@@ -5,7 +5,8 @@ with
     renamed as (
         select
           products_id, 
-          CAST(purchse_price as FLOAT64) as purchase_price
+          CAST(purchse_price as FLOAT64) as purchase_price,
+          CAST(purchase_cost as INT64) as purchase_cost
         from source
 
     )
