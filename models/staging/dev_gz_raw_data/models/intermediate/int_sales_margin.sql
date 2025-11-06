969a05568a99f564c03c125186799fa87a05ddb6
duval.ekaterina@gmail.com <duval.ekaterina@gmail.com>
2025-11-06 11:05:56 +0000

    calcul margin

--- models/staging/dev_gz_raw_data/models/intermediate/int_sales_margin.sql
+++ models/staging/dev_gz_raw_data/models/intermediate/int_sales_margin.sql
@@ -1,6 +1,6 @@
 select date_date, orders_id, quantity, purchase_price, revenue,   
-    round(quantity * purchase_price,2) as purchase_cost
-    revenue - purchase_cost as margin
+    round(quantity * purchase_price,2) as purchase_cost,
+    round(revenue - (quantity * purchase_price),2) as margin
 from {{ ref('stg_dev_gz_raw_data__sales') }}
 
 left join {{ ref('stg_dev_gz_raw_data__product') }}
